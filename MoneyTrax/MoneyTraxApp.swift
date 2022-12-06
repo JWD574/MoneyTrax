6f7548daeb89ae40909c30302e48b3fc4cb5217f
Joseph DeWeese <joe.deweese@me.com>
2022-12-06 09:07:37 -0600

    Initial Commit

---
+++ MoneyTrax/MoneyTraxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTraxApp.swift
+//  MoneyTrax
+//
+//  Created by Joseph  DeWeese on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTraxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
